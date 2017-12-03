c8232e45d74f1a64eef6c4a8b0cce98cbbcd0e55
Pavel Khrebto <rezgo.dev@dataart.com>
2017-12-03 16:52:59 +0100

    Add AppointmentEntityBuilder test

--- tests/unit/Builder/AppointmentEntityBuilderTest.php
+++ tests/unit/Builder/AppointmentEntityBuilderTest.php
@@ -3,9 +3,12 @@
 namespace App\Tests\Unit\Builder;
 
 use App\Builder\AppointmentEntityBuilder;
+use App\Entity\Appointment;
 use App\Entity\Hospital;
 use App\Entity\User;
 use Codeception\Test\Unit;
+use PHPUnit_Framework_MockObject_MockObject;
+use DateTime;
 
 /**
  * Class AppointmentEntityBuilderTest
@@ -14,11 +17,29 @@ use Codeception\Test\Unit;
  */
 class AppointmentEntityBuilderTest extends Unit
 {
+    /**
+     * @covers AppointmentEntityBuilder::build()
+     * @covers Appointment::getUser()
+     * @covers Appointment::getHospital()
+     * @covers Appointment::getAppointmentDatetime()
+     * @covers Appointment::setUser()
+     * @covers Appointment::setHospital()
+     * @covers Appointment::setAppointmentDatetime()
+     */
     public function testBuild() {
+        /** @var PHPUnit_Framework_MockObject_MockObject|User $userMock */
         $userMock = $this->getMockBuilder(User::class)->disableOriginalConstructor()->getMock();
+        /** @var PHPUnit_Framework_MockObject_MockObject|Hospital $hospitalMock */
         $hospitalMock = $this->getMockBuilder(Hospital::class)->disableOriginalConstructor()->getMock();
-        $date = '2017-12-25 08:00:00.000000';
+        $date = '2017-12-25 08:00:00';
 
         $appointmentEntityBuilder = new AppointmentEntityBuilder();
+        $appointmentEntity = $appointmentEntityBuilder->build($userMock, $hospitalMock, $date);
+
+        $this->assertInstanceOf(Appointment::class, $appointmentEntity);
+        $this->assertSame($userMock, $appointmentEntity->getUser());
+        $this->assertSame($hospitalMock, $appointmentEntity->getHospital());
+        $this->assertInstanceOf(DateTime::class, $appointmentEntity->getAppointmentDatetime());
+        $this->assertSame($date, $appointmentEntity->getAppointmentDatetime()->format("Y-m-d H:i:s"));
     }
 }
