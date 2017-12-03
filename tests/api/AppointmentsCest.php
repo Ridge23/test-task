5f8a3790aa1dc8b83c0d94ab4c614db7cef073ea
Pavel Khrebto <rezgo.dev@dataart.com>
2017-12-03 18:35:10 +0100

    add tests for api call for getting list of appointments

---
+++ tests/api/AppointmentsCest.php
@@ -0,0 +1,60 @@
+<?php
+
+
+class AppointmentsCest
+{
+    public function _before(ApiTester $I)
+    {
+        $I->am('Api tester');
+
+        $I->haveHttpHeader('Accept', 'application/json');
+        $I->haveHttpHeader('Content-Type', 'application/json');
+        $I->haveHttpHeader('apikey', 'test_api_key_1');
+    }
+
+    public function _after(ApiTester $I)
+    {
+    }
+
+    /**
+     * @param ApiTester $I
+     */
+    public function testGetAppointments(ApiTester $I)
+    {
+        $I->wantTo("Test getting of appointments");
+
+        $I->sendGet('appointments');
+        $I->seeResponseCodeIs(200);
+        $I->seeResponseIsJson();
+
+        $I->seeResponseMatchesJsonType(
+            [
+                'id' => 'integer',
+                'appointment_time' => [
+                    'date' => 'string',
+                    'timezone_type' => 'integer',
+                    'timezone' => 'string'
+                ],
+                'hospital' => [
+                    'name' => 'string',
+                    'city' => 'string',
+                    'country' => 'string'
+                ],
+                'user' => [
+                    'email' => 'string',
+                    'username' => 'string'
+                ],
+                'created_at' => [
+                    'date' => 'string',
+                    'timezone_type' => 'integer',
+                    'timezone' => 'string'
+                ],
+                'updated_at' => [
+                    'date' => 'string',
+                    'timezone_type' => 'integer',
+                    'timezone' => 'string'
+                ]
+            ]
+        );
+    }
+}
